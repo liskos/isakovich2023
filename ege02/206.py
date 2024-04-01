8bb83663f21205e3c83dc7c5779d126d6b1ca790
Maks <jjhgbmyhighly@gmail.com>
2024-04-01 15:53:30 +0300

    ..

---
+++ ege02/206.py
@@ -0,0 +1,7 @@
+for a in 0, 1:
+    for b in 0, 1:
+        for c in 0, 1:
+            for d in 0, 1:
+                        f = (not a or d) and not(not b or c)
+                        if f:
+                            print(a, b, c, d,"|", int(f))
---
+++ ege02/216.py
@@ -0,0 +1,7 @@
+for a in 0, 1:
+    for b in 0, 1:
+        for c in 0, 1:
+            for d in 0, 1:
+                        f = ((a and b) == (not c)) and (not b or d)
+                        if f:
+                            print(a, b, c,d,"|", int(f))
