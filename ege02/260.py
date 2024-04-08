f21034a68f9a7782af3c39162193ceecb20f3c15
Maks <jjhgbmyhighly@gmail.com>
2024-04-08 15:26:01 +0300

    ..

---
+++ ege02/260.py
@@ -0,0 +1,8 @@
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f = (not w or z) == (not y or x)
+                print(x, y, z, w, "|", int(f))
+                g = (not w or z) and (not x == y)
+                print(x, y, z, w, "|", int(f + 10))
---
+++ варианты/чайкин/вариант5/2.py
@@ -0,0 +1,10 @@
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f = (x or not y) and not(x == z) and w
+                if f:
+                    print(x, y, z, w, "|", int(f))
+                g = (not x or y) and (not y or z) and (not z or w)
+                if g:
+                    print(x, y, z, w, "|", "11")
