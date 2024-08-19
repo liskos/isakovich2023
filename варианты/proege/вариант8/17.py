c0e4c1bea3576dc6f69d80dd1c3fd1a229e27b49
kos <liskosnik@yandex.ru>
2024-08-19 18:46:11 +0300

    19/08/2024

--- варианты/proege/вариант8/17.py
+++ варианты/proege/вариант8/17.py
@@ -1,3 +1,14 @@
+def f(n):
+    if 1000 <= abs(n) < 10000 and n % 2 == 0:
+        return 1
+    else:
+        return 0
+
+
 c = []
 a = [int(x) for x in open('17.txt')]
-b =
+m3 = max([x for x in a if x % 9 == 3])
+for i in range(len(a) - 2):
+    if f(a[i]) + f(a[i+1]) + f(a[i+2]) <= 1 and a[i+1] + a[i+2] + a[i] <= m3:
+        c.append(a[i] + a[i+1] + a[i+2])
+print(len(c), max(c))
--- варианты/proege/вариант8/8.py
+++ варианты/proege/вариант8/8.py
@@ -1,7 +1,10 @@
 import itertools
 
 a = []
-for i in itertools.product('01234567', repeat=5):
-    if i.count('1') == 0 and i[0] != i[1] and i[1] != i[2] and i[2] != i[3] and i[3] != i[4] and int(i[0]) % 2 != int(i[1]) % 2 or int(i[1]) % 2 != int(i[2]) % 2 or int(i[2]) % 2 != int(i[3]) % 2 or int(i[3]) % 2 != int(i[4]) % 2:
+for i in itertools.product('0234567', repeat=5):
+    s = "".join(i)
+    s = s.replace("2", "0").replace("4", "0").replace("6", "0")
+    s = s.replace("5", "3").replace("7", "3")
+    if i[0] != '0' and len(set(i)) == 5 and ("00" not in s) and ("33" not in s):
         a.append(i)
 print(len(a))
