9bc0cd0a342bd30d132d3ac3eeca1c712da2c21b
kos <liskosnik@yandex.ru>
2025-01-04 16:16:26 +0300

    04/01/2025

--- варианты 2025/шастинибах/в7/26/a26.py
+++ варианты 2025/шастинибах/в7/26/a26.py
@@ -1,13 +1,37 @@
 def f(filename):
     file = open(filename)
-    n, s = map(int, file.readline().split())
+    n, s = map(int, file.readline().split()) # n - кол во мест в гард,  s  - человек
     print(n, s)
     l = []
     with file:
         for line in file:
             id, t, k = map(int, line.split())
             l.append([id, t, k])
+    l = sorted(l, key=lambda x: (x[1], x[0]))
     print(l)
+    sdani = []
+    ushli = []
+    count_number = n
+    summa = 0
+    a = []
+    for id, t, k in l:
+        if (id not in sdani) and (id not in ushli):
+            if count_number >= k:
+                sdani.append(id)
+                count_number -= k
+                if count_number == 0:
+                    a.append([t])
+            else:
+                ushli.append(id)
+                summa += k
+        elif (id in sdani) and (id not in ushli):
+            count_number += k
+            if a and len(a[-1])==1:
+                a[-1].append(t)
+    print(a)
+    return k, 0
+
+
 
 if __name__ == '__main__':
     print(f('26.txt'))
--- варианты 2025/шастинибах/в7/26/test26.py
+++ варианты 2025/шастинибах/в7/26/test26.py
@@ -1,4 +1,6 @@
 import a26
 
 def test1():
-    assert a26.f('test1.txt') == (4, 5)
+    assert a26.f('test1.txt') == (0, 6)
+def test2():
+    assert a26.f('test2.txt') == (4, 6)
